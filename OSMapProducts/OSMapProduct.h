f3259c65753aeea3bd981b4be85932af59ec2e24
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-04-08 10:36:47 +0100

    Add project

---
+++ OSMapProducts/OSMapProduct.h
@@ -0,0 +1,68 @@
+//
+//  OSMapProduct.h
+//  OSMapProducts
+//
+//  Created by David Haynes on 08/02/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  The available spatial references
+ */
+typedef NS_ENUM(NSUInteger, OSSpatialReference) {
+    /**
+     *  EPSG 27700 British National Grid
+     */
+    OSSpatialReferenceBNG,
+    /**
+     *  EPSG 3857 Web Mercator
+     */
+    OSSpatialReferenceWebMercator,
+};
+
+/**
+ *  The available basemap styles
+ */
+typedef NS_ENUM(NSUInteger, OSBaseMapStyle) {
+    /**
+     *  Road style
+     */
+    OSBaseMapStyleRoad,
+    /**
+     *  Outdoor style
+     */
+    OSBaseMapStyleOutdoor,
+    /**
+     *  Light style
+     */
+    OSBaseMapStyleLight,
+    /**
+     *  Night style
+     */
+    OSBaseMapStyleNight,
+    /**
+     *  Leisure map stack. Only available in BNG
+     */
+    OSBaseMapStyleLeisure
+};
+
+/**
+ *  Constructs the name to be used for the basemap layer in an http request.
+ *
+ *  @param style            The map style
+ *  @param spatialReference The map spatial reference
+ *
+ *  @return The name to be used for the layer.
+ */
+NSString *NSStringFromOSMapLayer(OSBaseMapStyle style, OSSpatialReference spatialReference);
+
+/**
+ *  Get the well known id from the spatial reference.
+ *
+ *  @param spatialReference The spatial reference
+ *
+ *  @return Well known id of the supplied spatial reference, e.g. '27700' for BNG
+ */
+NSInteger OSWkIDFromSpatialReference(OSSpatialReference spatialReference);
